84c72e3da605e4d31b2ad448f06abdb61dec4b2a
Evgeniy Petukhov <evgeniy.petukhov@gmail.com>
2022-08-07 13:46:35 +0200

    Added basic Telegram bot functionality

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,42 @@
 import { config } from "src/config/config";
+import { Telegraf } from "telegraf";
 
-console.log(config.GREETING);
+const bot = new Telegraf(config.BOT_TOKEN);
+
+let interval: NodeJS.Timer | undefined = undefined;
+
+bot.command(["ping", "start", "stop", "status"], (ctx) => {
+  if (ctx.message.from.id !== config.USER_ID) {
+    return;
+  }
+
+  if (ctx.message.text === "/ping") {
+    ctx.reply("pong");
+  } else if (ctx.message.text === "/start") {
+    ctx.reply("started");
+    interval = setInterval(() => {
+      ctx.reply(".");
+    }, 30000);
+  } else if (ctx.message.text === "/stop") {
+    ctx.reply("stopped");
+    clearInterval(interval);
+  } else if (ctx.message.text === "/status") {
+    ctx.reply(interval ? "running" : "stopped");
+  }
+});
+
+bot.on("text", (ctx) => {
+  if (ctx.message.from.id !== config.USER_ID) {
+    return;
+  }
+
+  ctx.reply(
+    `Hello ${ctx.message.from.username || ctx.message.from.first_name}`
+  );
+});
+
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv'
-import { object, string } from 'yup';
+import { number, object, string } from 'yup';
 
 const rawConfig = dotenv.config().parsed;
 
 const configSchema = object({
-  GREETING: string().required()
+  GREETING: string().required(),
+  BOT_TOKEN: string().required(),
+  USER_ID: number().required(),
 })
 
 export const config = configSchema.validateSync(rawConfig);
